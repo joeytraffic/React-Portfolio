d0476a679734288814e83c347cbff6d2cd25e0f2
Joseph <117396146+joeytraffic@users.noreply.github.com>
2023-06-18 15:20:57 -0700

    fixed issue where footer elements wouldn't open in new tab

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,13 +9,20 @@ const Footer = () => {
               <a href="mailto:joey.tranvik@gmail.com">joey.tranvik@gmail.com</a>
             </li>
             <li className="contact-link">
-              <a href="https://github.com/joeytraffic">GitHub</a>
+              <a href="https://github.com/joeytraffic" target="_blank">
+                GitHub
+              </a>
             </li>
             <li className="contact-link">
-              <a href="https://twitter.com/joeytraffic">Twitter</a>
+              <a href="https://twitter.com/joeytraffic" target="_blank">
+                Twitter
+              </a>
             </li>
             <li className="contact-link">
-              <a href="https://open.spotify.com/artist/0EwxM1Pv0584OF4jZsG8TI">
+              <a
+                href="https://open.spotify.com/artist/0EwxM1Pv0584OF4jZsG8TI"
+                target="_blank"
+              >
                 Spotify
               </a>
             </li>
